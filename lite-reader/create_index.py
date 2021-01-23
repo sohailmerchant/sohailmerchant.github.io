dc2a41323e6aa9f83bea5a1c32b4b6d87ad33e3d
S Merchant <sohailsmerchant@gmail.com>
2021-01-23 21:32:34 +0000

    sample files

---
+++ lite-reader/create_index.py
@@ -0,0 +1,79 @@
+import os
+
+path = r"/mnt/c/Development Work/sohailmerchant.github.io/lite-reader/"
+
+def html_open_tag():
+    html_opening_tag = """<html dir="rtl" lang="ar">
+                        <head>
+
+                            <meta charset="utf-8">
+
+                            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                            
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+                            <link rel="preconnect" href="https://fonts.gstatic.com">
+                            <link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet">
+                        
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+                            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+                            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+                            
+                            <link rel="stylesheet" type="text/css" href="css/style.css"  />
+                            <script type="text/javascript" src="js/CollapsibleLists.js"></script>
+                        
+                            
+                        </head>
+                        <body>
+                            <div class="container-fluid h-100">
+                                <!-- Navigation -->
+                        <nav class="navbar navbar-default">
+                            <div class="container-fluid">
+                            <div class="navbar-header">
+                                
+                                <a class="navbar-brand" href="http://www.kitab-project.org" target="_blank" style="margin-top:5px">
+                                    <img src="http://web.kitab-project.org/wp-content/uploads/2017/07/logo-small-2.png" width="80px"/></a>
+                            </div>
+                            
+                            </div>
+                        </nav>
+                        <div class='row p-30'>
+        <div class='col-md-12'>
+                            """
+    
+    return html_opening_tag
+
+def create_index(path):
+    index = ""
+    for root, dirs, files in os.walk(path):       
+        for name in files:
+            if name.endswith('html'):
+                #print(name)
+                path = "../lite-reader/"+name
+                index += """
+               
+                <p><a href='{}'>{}</a></p>
+                """.format(path,name)
+                #print(index)
+    return index
+
+def html_close():
+    html_closing_tag = """</div></div></div></body>"""
+    return html_closing_tag
+
+
+index = create_index(path)
+print(index)
+full_html = html_open_tag()
+full_html += index                         
+full_html += html_close()
+
+def create_html_file(html_str):
+    print("SAVING AS", "index.html")
+    with open("index.html", "w", encoding="utf-8") as e:
+        #e.write(html_open())
+        e.write(html_str)
+        #e.write(html_close())
+        
+create_html_file(full_html)
